8a442915007a80c7992b82731bac71e1b369cbe1
Nahid2002S <viptechmaster@gmail.com>
2023-05-01 21:54:30 +0600

    vercel json added

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+var cors = require('cors')
+
+app.use(cors())
+
 app.get('/', (req, res) => {
   res.send('Tokyo Kitchen Server is Running')
 })
